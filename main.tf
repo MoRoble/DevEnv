1b8b2f1e11587ad1dbf04f0780a741b83377c500
MoRoble <m_roble@outlook.com>
2022-09-17 23:40:48 +0100

    Subnet numbers fixed

--- main.tf
+++ main.tf
@@ -5,9 +5,9 @@ module "networking" {
   pub_sn_count = 3
   app_sn_count = 3 #var.app_account
   db_sn_count  = 3
-  pub_cidrs    = [for i in range(1, 6, 2) : cidrsubnet(local.vpc_cidr, 8, i)]
-  app_cidrs    = [for i in range(2, 7, 2) : cidrsubnet(local.vpc_cidr, 8, i)]
-  db_cidrs     = [for i in range(3, 8, 2) : cidrsubnet(local.vpc_cidr, 8, i)]  
+  pub_cidrs    = [for i in range(4) : cidrsubnet(local.vpc_cidr, 4, i)]
+  app_cidrs    = [for i in range(4, 8) : cidrsubnet(local.vpc_cidr, 4, i)]
+  db_cidrs     = [for i in range(8, 12) : cidrsubnet(local.vpc_cidr, 4, i)]
   max_subnets  = 10
   access_ip    = var.access_ip
   # r_access_ip     = var.access_ip
