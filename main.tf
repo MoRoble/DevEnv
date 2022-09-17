c2d2b36dde75ba596027de60246db0f11e30116c
MoRoble <m_roble@outlook.com>
2022-09-17 23:21:52 +0100

    fixed sunbetting issues
    there's specific version of tf used?, the S3 bucket name giving unnecessary errors

--- main.tf
+++ main.tf
@@ -5,9 +5,9 @@ module "networking" {
   pub_sn_count = 3
   app_sn_count = 3 #var.app_account
   db_sn_count  = 3
-  pub_cidrs    = [for i in range(1, 70, 2) : cidrsubnet(local.vpc_cidr, 8, i)]
-  app_cidrs    = [for i in range(72, 90, 2) : cidrsubnet(local.vpc_cidr, 8, i)]
-  db_cidrs     = [for i in range(94, 255, 2) : cidrsubnet(local.vpc_cidr, 8, i)]  
+  pub_cidrs    = [for i in range(4) : cidrsubnet(local.vpc_cidr, 4, i)]
+  app_cidrs    = [for i in range(4, 8) : cidrsubnet(local.vpc_cidr, 4, i)]
+  db_cidrs     = [for i in range(8, 12) : cidrsubnet(local.vpc_cidr, 4, i)]
   max_subnets  = 10
   access_ip    = var.access_ip
   # r_access_ip     = var.access_ip
@@ -26,7 +26,8 @@ module "compute" {
   instance_count  = 1
   instance_type   = "t3.micro"
   vol_size        = "20"
-  public_key_path = "/Users/hamdi.hassan/terraform-practice/DevEnv/devenv.pub"
+  public_key_path = "/Users/Mohamed.Roble/Documents/Dev/DevEnv/devenv.pub"
+  # public_key_path = "/Users/hamdi.hassan/terraform-practice/DevEnv/devenv.pub"
   key_name        = "devenv"
   # instance_profile = "dev_profile"
 
--- providers.tf
+++ providers.tf
@@ -19,5 +19,5 @@ terraform {
 provider "aws" {
   region  = var.aws_region
   shared_credentials_files = ["~/.aws/credentials"]
-  profile                  = "aws-devenv"
+  profile                  = "devenv01"
 }
