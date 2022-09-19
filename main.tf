0bb9bf412b9ef07ca081ac4a54b52c1e27841a3f
MoRoble <m_roble@outlook.com>
2022-09-19 19:28:10 +0100

    fixed region
    s3 was't able to create due setting missconfigurations

--- main.tf
+++ main.tf
@@ -61,7 +61,7 @@ module "lb" {
   source = "./lb"
   # lb_count = 1
   lb_security_group       = module.networking.security_group_wordpress
-  lb_public_subnets          = module.networking.public_subnets
+  lb_public_subnets       = module.networking.public_subnets
   tg_port                 = 8000
   tg_protocol             = "HTTP"
   vpc_id                  = module.networking.vpc_id
